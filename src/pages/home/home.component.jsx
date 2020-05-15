3ac8c89011e5f0a0d800e407df1dd8bb95969489
Muddassar Shaikh <shaikh.muddassar8@gmail.com>
2020-05-15 17:39:24 +0530

    Added styled components

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 // Import Styles
-import './home.styles.scss';
+import { HomeContainer } from './home.styles';
 
 // Import Components
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => (
-  <div className="homepage">
+  <HomeContainer>
     <Directory />
-  </div>
+  </HomeContainer>
 );
 
 export default Home;
